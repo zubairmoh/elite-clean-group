c4ead7eabdd70e82a33bf24d9658e911f3872af7
zubairmoh <56039357+zubairmoh@users.noreply.github.com>
2026-01-03 11:22:26 -0500

    Update route.js

--- src/app/api/leads/update/route.js
+++ src/app/api/leads/update/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import db from '@/lib/db';
+import db from '../../../lib/db';
 
 export async function POST(request) {
   const { id, status } = await request.json();
